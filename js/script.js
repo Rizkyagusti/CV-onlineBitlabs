8b88d3415fbdebd761bb636b32c67ad26ed362d6
Rizky Agusti <rizkyagusti7@gmail.com>
2020-05-01 14:55:44 +0700

    Menginput file ke repo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// efek parallax scroll
+
+
+$(window).scroll(function(){
+    
+    var winscroll=$(this).scrollTop();
+
+
+    $('.jumbotron img').css({
+
+        'tramsform':translate(0px,)
+
+    });
+
+
+});
